3cf5d8ce4a74104220724bd960385cd72fbf68a7
Adam Kerrigan <ak1014@york.ac.uk>
2020-09-09 02:29:22 +0100

    Added cube file support, closing Issue #3. Also re-jigged arguments to work better with unit testing and added those tests. Added the -J, --thread argument for controlling the threads used by the program.

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,6 +1,6 @@
 use crate::io::{self, ReadFunction};
 use crate::methods::{self, StepMethod};
-use clap::{crate_authors, App, Arg};
+use clap::{crate_authors, App, Arg, ArgMatches};
 
 /// Indicates how many reference files are passed
 #[derive(Clone)]
@@ -10,19 +10,15 @@ pub enum Reference {
     None,
 }
 
-/// Holds the arguments passed to the program from the command-line
-pub struct Args {
-    pub file: String,
-    pub read: ReadFunction,
-    pub method: StepMethod,
-    pub reference: Reference,
-    pub vacuum_tolerance: Option<f64>,
+/// Create a container for dealing with clap and being able to test arg parsing
+pub enum ClapApp {
+    App,
 }
 
-impl Args {
-    /// Initialises the structure from the command-line arguments.
-    pub fn new() -> Self {
-        let arguments = App::new("Multi-threaded Bader Charge Analysis")
+impl ClapApp {
+    /// Create and return the clap::App
+    pub fn get(&self) -> App {
+        App::new("Multi-threaded Bader Charge Analysis")
             .author(crate_authors!())
             .version("0.1.0")
             .arg(Arg::with_name("file")
@@ -45,7 +41,7 @@ J. Phys.: Condens. Matter 21, 084204 (2009)"))
                 .long("type")
                 .takes_value(true)
                 .possible_value("cube")
-                .possible_value("chgcar")
+                .possible_value("vasp")
                 .case_insensitive(false)
                 .about("the file type of the charge density")
                 .long_about(
@@ -77,8 +73,32 @@ passed they are summed together."))
                 .long_about(
 "Values of density below the supplied value are considered vacuum and are not
 included in the calculation. A value of \"auto\" can be passed to use 1E-3 C*m^-3"))
-            .get_matches();
+            .arg(Arg::with_name("threads")
+                .short('J')
+                .long("threads")
+                .takes_value(true)
+                .default_value("0")
+                .about("Number of threads to distribute the calculation over.")
+                .long_about(
+"The number of threads to be used by the program. A default value of 0 is used
+to allow the program to best decide how to use the available hardware."))
+    }
+}
 
+/// Holds the arguments passed to the program from the command-line
+pub struct Args {
+    pub file: String,
+    pub read: ReadFunction,
+    pub method: StepMethod,
+    pub reference: Reference,
+    pub threads: usize,
+    pub vacuum_tolerance: Option<f64>,
+    pub zyx_format: bool,
+}
+
+impl Args {
+    /// Initialises the structure from the command-line arguments.
+    pub fn new(arguments: ArgMatches) -> Self {
         let file = match arguments.value_of("file") {
             Some(f) => String::from(f),
             None => String::new(),
@@ -87,12 +107,41 @@ included in the calculation. A value of \"auto\" can be passed to use 1E-3 C*m^-
             Some(f) => Some(String::from(f)),
             None => None,
         };
+        let mut zyx_format = false;
+        let read: ReadFunction = match file_type {
+            Some(ftype) => {
+                if ftype.eq("cube") {
+                    io::cube::read
+                } else {
+                    zyx_format = true;
+                    io::vasp::read
+                }
+            }
+            None => {
+                if file.to_lowercase().contains("cube") {
+                    io::cube::read
+                } else if file.to_lowercase().contains("car") {
+                    zyx_format = true;
+                    io::vasp::read
+                } else {
+                    println!("Error: File-type cannot be infered, attempting to read as VASP");
+                    zyx_format = true;
+                    io::vasp::read
+                }
+            }
+        };
         let method = match arguments.value_of("method") {
             Some("neargrid") => methods::neargrid,
             Some("ongrid") => methods::ongrid,
             Some(_) => methods::neargrid,
             None => methods::neargrid,
         };
+        // safe to unwrap as threads has a default value of 0
+        let threads =
+            match arguments.value_of("threads").unwrap().parse::<usize>() {
+                Ok(x) => x,
+                Err(e) => panic!("Couldn't parse threads into integer:\n{}", e),
+            };
         let vacuum_tolerance = match arguments.value_of("vacuum tolerance") {
             Some(s) => {
                 if s.eq("auto") {
@@ -100,8 +149,8 @@ included in the calculation. A value of \"auto\" can be passed to use 1E-3 C*m^-
                 } else {
                     match s.parse::<f64>() {
                         Ok(x) => Some(x),
-                        Err(_) => {
-                            panic!("Couldn't parse vacuum tolerance: {}", s)
+                        Err(e) => {
+                            panic!("Couldn't parse vacuum tolerance into float:\n{}", e)
                         }
                     }
                 }
@@ -109,6 +158,9 @@ included in the calculation. A value of \"auto\" can be passed to use 1E-3 C*m^-
             None => None,
         };
         let references: Vec<_> = if arguments.is_present("all electron") {
+            if read as usize != io::vasp::read as usize {
+                panic!("Error: Cannot use AECCAR flag for non VASP file-types.")
+            }
             vec!["AECCAR0", "AECCAR2"]
         } else {
             match arguments.values_of("reference") {
@@ -122,27 +174,257 @@ included in the calculation. A value of \"auto\" can be passed to use 1E-3 C*m^-
             1 => Reference::One(String::from(references[0])),
             _ => Reference::None,
         };
-        let read: ReadFunction = match file_type {
-            Some(s) => {
-                if s.eq("vasp") {
-                    io::vasp::read
-                } else {
-                    io::vasp::read
-                }
-            }
-            None => {
-                if file.to_lowercase().contains("car") {
-                    io::vasp::read
-                } else {
-                    println!("Error: File-type cannot be infered, attempting to read as VASP");
-                    io::vasp::read
-                }
-            }
-        };
         return Self { file,
                       read,
                       method,
                       reference,
-                      vacuum_tolerance };
+                      threads,
+                      vacuum_tolerance,
+                      zyx_format };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn clapapp_get() {
+        let app = ClapApp::App.get();
+        assert_eq!(app.get_name(), "Multi-threaded Bader Charge Analysis")
+    }
+
+    #[test]
+    fn argument_file() {
+        let app = ClapApp::App.get();
+        let matches = app.get_matches_from(vec!["bader", "CHGCAR"]);
+        let args = Args::new(matches);
+        assert_eq!(args.file, String::from("CHGCAR"));
+    }
+
+    #[test]
+    #[should_panic]
+    fn argument_no_file() {
+        let app = ClapApp::App.get();
+        let _ = app.try_get_matches_from(vec!["bader"])
+                   .unwrap_or_else(|e| panic!("An error occurs: {}", e));
+    }
+
+    #[test]
+    fn argument_method_ongrid() {
+        let app = ClapApp::App.get();
+        let matches =
+            app.get_matches_from(vec!["bader", "CHGCAR", "-m", "ongrid"]);
+        let args = Args::new(matches);
+        let method: StepMethod = methods::ongrid;
+        assert_eq!(args.method as usize, method as usize);
+    }
+
+    #[test]
+    fn argument_method_neargrid() {
+        let app = ClapApp::App.get();
+        let matches = app.get_matches_from(vec!["bader", "CHGCAR",
+                                                "--method", "neargrid"]);
+        let args = Args::new(matches);
+        let method: StepMethod = methods::neargrid;
+        assert_eq!(args.method as usize, method as usize);
+    }
+
+    #[test]
+    fn argument_method_default() {
+        let app = ClapApp::App.get();
+        let matches = app.get_matches_from(vec!["bader", "CHGCAR"]);
+        let args = Args::new(matches);
+        let method: StepMethod = methods::neargrid;
+        assert_eq!(args.method as usize, method as usize);
+    }
+
+    #[test]
+    #[should_panic]
+    fn argument_method_not_method() {
+        let app = ClapApp::App.get();
+        let _ = app.try_get_matches_from(vec!["bader", "CHGCAR", "-m",
+                                              "ngrid"])
+                   .unwrap_or_else(|e| panic!("An error occurs: {}", e));
+    }
+
+    #[test]
+    fn argument_file_type_default_vasp() {
+        let app = ClapApp::App.get();
+        let matches = app.get_matches_from(vec!["bader", "CHGCAR"]);
+        let args = Args::new(matches);
+        let read: ReadFunction = io::vasp::read;
+        assert_eq!(args.read as usize, read as usize);
+    }
+
+    #[test]
+    fn argument_file_type_default_unknown() {
+        let app = ClapApp::App.get();
+        let matches = app.get_matches_from(vec!["bader", "CHG"]);
+        let args = Args::new(matches);
+        let read: ReadFunction = io::vasp::read;
+        assert_eq!(args.read as usize, read as usize);
+    }
+
+    #[test]
+    fn argument_file_type_vasp() {
+        let app = ClapApp::App.get();
+        let matches =
+            app.get_matches_from(vec!["bader", "CHGCAR", "-t", "vasp"]);
+        let args = Args::new(matches);
+        let read: ReadFunction = io::vasp::read;
+        assert_eq!(args.read as usize, read as usize);
+    }
+
+    #[test]
+    fn argument_file_type_default_cube() {
+        let app = ClapApp::App.get();
+        let matches = app.get_matches_from(vec!["bader", "charge.cube"]);
+        let args = Args::new(matches);
+        let read: ReadFunction = io::cube::read;
+        assert_eq!(args.read as usize, read as usize);
+    }
+
+    #[test]
+    fn argument_file_type_cube() {
+        let app = ClapApp::App.get();
+        let matches = app.get_matches_from(vec!["bader",
+                                                "charge.cube",
+                                                "--type",
+                                                "cube"]);
+        let args = Args::new(matches);
+        let read: ReadFunction = io::cube::read;
+        assert_eq!(args.read as usize, read as usize);
+    }
+
+    #[test]
+    #[should_panic]
+    fn argument_file_type_not_type() {
+        let app = ClapApp::App.get();
+        let _ = app.try_get_matches_from(vec!["bader", "CHGCAR", "-t", "basp"])
+                   .unwrap_or_else(|e| panic!("An error occurs: {}", e));
+    }
+
+    #[test]
+    fn argument_reference_one() {
+        let app = ClapApp::App.get();
+        let matches =
+            app.get_matches_from(vec!["bader", "CHGCAR", "-r", "CHGCAR_sum"]);
+        let args = Args::new(matches);
+        let flag = match args.reference {
+            Reference::One(_) => true,
+            _ => false,
+        };
+        assert!(flag)
+    }
+
+    #[test]
+    fn argument_reference_two() {
+        let app = ClapApp::App.get();
+        let v = vec!["bader", "CHGCAR", "-r", "AECCAR0", "--ref", "AECCAR2"];
+        let matches = app.get_matches_from(v);
+        let args = Args::new(matches);
+        let flag = match args.reference {
+            Reference::Two(_, _) => true,
+            _ => false,
+        };
+        assert!(flag)
+    }
+
+    #[test]
+    fn argument_reference_none() {
+        let app = ClapApp::App.get();
+        let v = vec!["bader", "CHGCAR"];
+        let matches = app.get_matches_from(v);
+        let args = Args::new(matches);
+        let flag = match args.reference {
+            Reference::None => true,
+            _ => false,
+        };
+        assert!(flag)
+    }
+
+    #[test]
+    fn argument_aeccar() {
+        let app = ClapApp::App.get();
+        let v = vec!["bader", "CHGCAR", "-a"];
+        let matches = app.get_matches_from(v);
+        let args = Args::new(matches);
+        let flag = match args.reference {
+            Reference::Two(x, y) => {
+                if (x == String::from("AECCAR0"))
+                   && (y == String::from("AECCAR2"))
+                {
+                    true
+                } else {
+                    false
+                }
+            }
+            _ => false,
+        };
+        assert!(flag)
+    }
+
+    #[test]
+    #[should_panic]
+    fn argument_aeccar_cube() {
+        let app = ClapApp::App.get();
+        let v = vec!["bader", "charge.cube", "-a"];
+        let matches = app.get_matches_from(v);
+        let _ = Args::new(matches);
+    }
+
+    #[test]
+    fn argument_vacuum_tolerance_auto() {
+        let app = ClapApp::App.get();
+        let v = vec!["bader", "CHGCAR", "-v", "auto"];
+        let matches = app.get_matches_from(v);
+        let args = Args::new(matches);
+        assert_eq!(args.vacuum_tolerance, Some(1E-3))
+    }
+
+    #[test]
+    fn argument_vacuum_tolerance_float() {
+        let app = ClapApp::App.get();
+        let v = vec!["bader", "CHGCAR", "--vac", "1E-4"];
+        let matches = app.get_matches_from(v);
+        let args = Args::new(matches);
+        assert_eq!(args.vacuum_tolerance, Some(1E-4))
+    }
+
+    #[test]
+    #[should_panic]
+    fn argument_vacuum_tolerance_not_float() {
+        let app = ClapApp::App.get();
+        let v = vec!["bader", "CHGCAR", "-v", "0.00.1"];
+        let matches = app.get_matches_from(v);
+        let _ = Args::new(matches);
+    }
+
+    #[test]
+    fn argument_threads_default() {
+        let app = ClapApp::App.get();
+        let v = vec!["bader", "CHGCAR"];
+        let matches = app.get_matches_from(v);
+        let args = Args::new(matches);
+        assert_eq!(args.threads, 0)
+    }
+
+    #[test]
+    fn argument_threads_int() {
+        let app = ClapApp::App.get();
+        let v = vec!["bader", "CHGCAR", "--threads", "1"];
+        let matches = app.get_matches_from(v);
+        let args = Args::new(matches);
+        assert_eq!(args.threads, 1)
+    }
+
+    #[test]
+    #[should_panic]
+    fn argument_threads_not_int() {
+        let app = ClapApp::App.get();
+        let v = vec!["bader", "CHGCAR", "-J", "0.1"];
+        let matches = app.get_matches_from(v);
+        let _ = Args::new(matches);
     }
 }
