4aedda66c8e13445d4ead6f527ab6620b0096847
Adam <adam.kerrigan@york.ac.uk>
2023-08-08 18:00:46 +0100

    release checklist

--- src/arguments.rs
+++ src/arguments.rs
@@ -20,7 +20,7 @@ impl ClapApp {
     pub fn get() -> Command {
         Command::new("Multi-threaded Bader Charge Analysis")
             .author(crate_authors!())
-            .version("0.4.4")
+            .version("0.4.5")
             .arg(Arg::new("file")
                 .required(true)
                 .index(1)
